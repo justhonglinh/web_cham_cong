1fc6992f3864b46322e9d3d77bcdd35643f4fbde
linh <linhnhok119@gmail.com>
2025-05-26 16:33:11 +0700

    26/5

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,12 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\FaceCompareController;
+
+// route call api so sánh khuôn mặt
+Route::get('/face-compare', [FaceCompareController::class, 'showForm']);
+Route::post('/face-compare', [FaceCompareController::class, 'compare']);
+
 // giao diện ban đầu
 Route::get('/', function () {
     return view('welcome');
