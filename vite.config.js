226f3a6a61a7af37df058ecc1b749f04e3f2a4e7
Linh <linhnhok119@gmail.com>
2025-05-27 22:21:32 +0700

    23/05

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,6 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    server: {
-        host: '0.0.0.0',   // Cho phép truy cập từ bên ngoài localhost
-        port: 5173,        // Port mặc định Vite, bạn có thể đổi nếu muốn
-        strictPort: true,  // Nếu port đang dùng thì lỗi, không tự động đổi port
-    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
