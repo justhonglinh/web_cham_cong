aadd20fbc4005dc410565bade590696056b6daaa
Linh <linhhd@skylab.vn>
2026-07-01 18:22:17 +0700

    1/7

--- be/bootstrap/app.php
+++ be/bootstrap/app.php
@@ -16,7 +16,6 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'role' => RoleMiddleware::class,
         ]);
-        $middleware->statefulApi();
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
